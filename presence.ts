91725fac0f3cdc287af862b1b7451716db8a9389
luisgabriel53 <harrycorpmail@gmail.com>
2020-09-22 16:38:42 -0300

    atualizando

--- presence.ts
+++ presence.ts
@@ -1,24 +1,39 @@
-var presence = new Presence({
+const presence = new Presence({
     clientId: "756196794727399617"
-}), strings = presence.getStrings({
-    play: "presence.playback.playing",
-    pause: "presence.playback.paused"
 });
-var browsingStamp = Math.floor(Date.now() / 1000);
+
 presence.on("UpdateData", async () => {
 
-var presenceData = {
-        largeImageKey: "large_image",
-        smallImageKey: "playing",
-        smallImageText: "Playing",
-        details: "In game",
-        startTimestamp: browsingStamp
-            } 
-    if (presenceData.details == null) {
-        presence.setTrayTitle();
-        presence.setActivity();
-    }
-    else {
-        presence.setActivity(presenceData);
-    }
+    const browsingStamp = Math.floor(Date.now() / 1000), 
+          button = document.getElementsByTagName('button'), 
+          valor = button.length, 
+          players = document.getElementsByClassName('userActive'), 
+          nump = `(${players.length} of 6 players)`,
+          data: PresenceData = {
+            largeImageKey: "large_image",
+            startTimestamp: browsingStamp
+                };
+
+        if (valor == 1) {
+            data.details = "Creating a Room";
+            data.smallImageKey = "home";
+            data.smallImageText = "On homepage";
+        }
+        if (valor >= 6) {
+            data.details = "Playing";
+            data.state = nump;
+            data.smallImageKey = "playing";
+            data.smallImageText = "On game";
+            data.startTimestamp = browsingStamp;
+        }
+        if (players.length > 6) {
+            data.state = "(6 of 6 players)";
+        }
+        if (data.details == null) {
+            presence.setTrayTitle();
+            presence.setActivity();
+        }
+        else {
+            presence.setActivity(data);
+        }
 });
